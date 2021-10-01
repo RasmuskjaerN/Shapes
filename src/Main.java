2bcbc5b60526479e12744aa03305c7cd243f7a1c
Marcus Hinrichsen <marcushinm@hotmail.dk>
2021-10-01 23:08:41 +0200

    Triangle made

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,12 @@ public class Main {
 
         Rectangle rect = new Rectangle(1,1,3,3);
         System.out.println("Rectangle Area is " + rect.area());
-        System.out.println("Rectangle Circumfrence is "+rect.circumference());
+        System.out.println("Rectangle Circumference is "+rect.circumference());
         System.out.println("Rectangle Center is "+rect.centerX() + ", "+ rect.centerY());
+        System.out.println();
+
+        Triangle tria = new Triangle(11,11,22,22,33,33);
+        System.out.println(tria.isInside());
         }
         //branch test
 
--- src/Triangle.java
+++ src/Triangle.java
@@ -1,47 +1,70 @@
 public class Triangle extends Shapes{
 
-    public Triangle() {
+    public Triangle(double x1, double y1, double x2, double y2, double x3, double y3) {
+        //first point
+        this.x1 =x1;
+        this.y1 =y1;
+        //second point
+        this.x2 =x2;
+        this.y2 =y2;
+        //third point
+        this.x3 =x3;
+        this.y3 =y3;
 
     }
 
 
     @Override
     public double side1() {
-        return 0;
+        double xSide = Math.pow(((x2)-(x1)),2);
+        double ySide = Math.pow(((y2)-(y1)),2);
+        return Math.sqrt((xSide+ySide));
     }
 
     @Override
     public double side2() {
-        return 0;
+        double xSide2 = Math.pow(((x3)-(x2)),2);
+        double ySide2 = Math.pow(((y3)-(y2)),2);
+
+        return Math.sqrt((xSide2+ySide2));
     }
 
     @Override
     public double side3() {
-        return 0;
+        double xSide3 = Math.pow(((x1)-(x3)),2);
+        double ySide3 = Math.pow(((y1)-(y3)),2);
+        return Math.sqrt((xSide3+ySide3));
     }
 
     @Override
     public double area() {
-        return 0;
+        return Math.abs((x1*(y2-y3) + x2*(y3-y1)+x3*(y1-y2))/2.0);
     }
 
     @Override
     public double circumference() {
-        return 0;
+        return side1()+side2()+side3();
     }
 
     @Override
     public boolean isInside() {
-        return false;
+
+        double d = ((y2 - y3)*(x1 - x3) + (x3 - x2)*(y1 - y3));
+        double a = ((y2 - y3)*(x1 - x3) + (x3 - x2)*(y1 - y3)) / d;
+        double b = ((y3 - y1)*(x1 - x3) + (x1 - x3)*(y1 - y3)) / d;
+        double c = 1 - a - b;
+
+        return 0 <= a && a <= 1 && 0 <= b && b <= 1 && 0 <= c && c <= 1;
+
     }
 
     @Override
     public double centerX() {
-        return 0;
+        return (x3 + x2 + x1)/3;
     }
 
     @Override
     public double centerY() {
-        return 0;
+        return (y3+y2+y1)/3;
     }
 }
