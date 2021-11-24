816e25bbfae61e04c9c0fea7ecf5ebeb8687c333
Marcus Hinrichsen <marcushinm@hotmail.dk>
2021-11-24 01:35:44 +0100

    fix

--- Test/RectangleTest.java
+++ Test/RectangleTest.java
@@ -2,13 +2,14 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 class RectangleTest {
+    //create a test object
+    Rectangle rect = new Rectangle(4.0, 6.0, 2, 3);
 
-    @Test
-    void newRectangleSideTest(){
-        Rectangle TestRectangle=new Rectangle(1,1,3,3);
-        assertEquals(2,TestRectangle.side1());
+    @org.junit.jupiter.api.Test
+    void circumfernce() {
+        assertEquals(rect.circumference(), 20);
     }
-
-
 }
